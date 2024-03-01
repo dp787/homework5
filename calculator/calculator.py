402f830622a22115be100edf924f9f2f4cc70136
Devendra Pitam <90913482+dp787@users.noreply.github.com>
2024-03-01 15:27:51 -0500

    Update calculator.py

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -31,6 +31,20 @@ class Calculator:
         for command, description in cls.commands.items():
             print(f"{command}: {description}")
 
-if __name__ == "__main__":
-    Calculator.display_menu()
+    @classmethod
+    def execute_command(cls, command):
+        if command not in cls.commands:
+            raise ValueError("Invalid command")
 
+        if command == 'add':
+            return cls.add(2, 3)
+        elif command == 'subtract':
+            return cls.subtract(5, 3)
+        elif command == 'multiply':
+            return cls.multiply(2, 3)
+        elif command == 'divide':
+            return cls.divide(6, 2)
+        elif command == 'menu':
+            cls.display_menu()
+        else:
+            raise ValueError("Invalid command")
